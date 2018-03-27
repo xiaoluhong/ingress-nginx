b7b4c4f83e926741702e5e393c5b4ca281b41640
Yuxing <yuxing@rancher.com>
2018-03-26 17:45:45 -0700

    Add RKE Annotation for external and internal ip
    Related issue https://github.com/rancher/rke/pull/444 and
    https://github.com/rancher/rancher/issues/12169
    Since RKE can not set external ip for the node, we need to select from
    node annotation.
    So when we choose to report external ip, we will follow this order:
    1. node.status.addresses.externalIp
    2. node.metadata.annotations["rke.cattle.io/external-ip"]
    3. node.metadata.annotations["rke.cattle.io/internal-ip"]

--- internal/k8s/main.go
+++ internal/k8s/main.go
@@ -28,6 +28,11 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+const (
+	internalAddressAnnotation = "rke.cattle.io/internal-ip"
+	externalAddressAnnotation = "rke.cattle.io/external-ip"
+)
+
 // ParseNameNS parses a string searching a namespace and name
 func ParseNameNS(input string) (string, string, error) {
 	nsName := strings.Split(input, "/")
@@ -51,6 +56,14 @@ func GetNodeIPOrName(kubeClient clientset.Interface, name string, useInternalIP 
 				if address.Address != "" {
 					return address.Address
 				}
+			}
+		}
+		if node.Annotations != nil {
+			if annotatedIP := node.Annotations[externalAddressAnnotation]; annotatedIP != "" {
+				return annotatedIP
+			}
+			if annotatedIP := node.Annotations[internalAddressAnnotation]; annotatedIP != "" {
+				return annotatedIP
 			}
 		}
 	}
